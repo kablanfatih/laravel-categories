1d885f68da20fd28bf47972177984ea501472bd7
Brian Faust <faustbrian@users.noreply.github.com>
2018-03-20 17:57:18 +0200

    remove old methods from trait

--- src/Traits/HasCategories.php
+++ src/Traits/HasCategories.php
@@ -19,9 +19,6 @@ use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 trait HasCategories
 {
-    /**
-     * @return mixed
-     */
     public function categories(): MorphToMany
     {
         return $this->morphToMany(
@@ -31,62 +28,11 @@ trait HasCategories
         );
     }
 
-    /**
-     * @return mixed
-     */
     public function categoriesList(): array
     {
         return $this->categories()
                     ->pluck('name', 'id')
                     ->toArray();
-    }
-
-    /**
-     * @param $categories
-     */
-    public function categorize($categories)
-    {
-        foreach ($categories as $category) {
-            $this->addCategory($category);
-        }
-    }
-
-    /**
-     * @param $categories
-     */
-    public function uncategorize($categories)
-    {
-        foreach ($categories as $category) {
-            $this->removeCategory($category);
-        }
-    }
-
-    /**
-     * @param $categories
-     */
-    public function recategorize($categories)
-    {
-        $this->categories()->sync([]);
-
-        $this->categorize($categories);
-    }
-
-    /**
-     * @param Model $category
-     */
-    public function addCategory(Model $category)
-    {
-        if (!$this->categories->contains($category->getKey())) {
-            $this->categories()->attach($category);
-        }
-    }
-
-    /**
-     * @param Model $category
-     */
-    public function removeCategory(Model $category)
-    {
-        $this->categories()->detach($category);
     }
 
     public function assignCategory(...$categories)
