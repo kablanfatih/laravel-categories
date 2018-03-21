ba77fcbb58cc53ba6cbed9eedd20cfa42e22cfed
Brian Faust <faustbrian@users.noreply.github.com>
2018-03-21 05:45:56 +0200

    publish migration with date prefix

--- src/CategoriesServiceProvider.php
+++ src/CategoriesServiceProvider.php
@@ -20,7 +20,7 @@ class CategoriesServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
+            __DIR__.'/../database/migrations/create_categories_tables.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_categories_tables.php'),
         ], 'migrations');
 
         $this->publishes([
